4cbee67a7c5c80514ebc279ac06167cf1114dec4
Marsel Usmanov <usmanovmk@gmail.com>
2019-01-28 01:04:35 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,11 @@
 function load_curr_rate() {
 	xhr.open('GET', 'ajax.php');
 	xhr.onload = function () {
-		var json = JSON.parse(xhr.responseText)
-		//console.log(json);
-		$('#euro').text(json.Nominal);
-		$('#rub').text(json.Value);
+		if (var json = JSON.parse(xhr.responseText)) {
+			//console.log(json);
+			$('#euro').text(json.Nominal);
+			$('#rub').text(json.Value);
+		}
 		setInterval(load_curr_rate, 10000);
 	};
 	xhr.send();
